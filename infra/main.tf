3a7df27637739b2b0d9fa394b09f81b91392b219
Marcos Raia <79171007+MarcosRaia@users.noreply.github.com>
2024-09-28 14:07:55 +0000

    commit6

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
-module "bucket"{
-    source = "./modules/ec2-aws"
-    bucket_prefix = "terraform-bucket-posmack"
-    region = "us-east-2"
-
+module "bucket" {
+  source      = "./modules/ec2-aws"
+  instance_name = var.instance_name
+  ami_id       = var.ami_id
+  instance_type = var.instance_type
+  bucket_prefix = var.bucket_prefix
 }
