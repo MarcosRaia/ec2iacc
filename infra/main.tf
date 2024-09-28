1ffe03b765e842e5f909bc37d56d92b652b15f7c
Marcos Raia <79171007+MarcosRaia@users.noreply.github.com>
2024-09-28 14:12:02 +0000

    Commit8

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "bucket" {
-  source      = "./infra/prod"
+  source      = "./modules/ec2-aws"
   instance_name = var.instance_name
   ami_id       = var.ami_id
   instance_type = var.instance_type
